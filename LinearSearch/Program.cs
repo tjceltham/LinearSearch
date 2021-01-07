fb73c8b4a83c9baec571930057282cf384e687d9
Tony Collins <tjc@eltham-college.org.uk>
2021-01-07 12:17:12 +0000

    Add project files.

---
+++ LinearSearch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = { 1, 4, 345, 6, 7, 8, 24, 6, 7 };
+            int num = 0;
+            string numStr = "";
+            Console.WriteLine("Enter the number you would like to search for");
+            numStr = Console.ReadLine();
+            num = Convert.ToInt32(numStr);
+
+            // add Linear search code here
+            // try to use a while loop ratherthan a for loop to improve the efficency of you code
+            // Print out a message stating if the number is in the list and if it is 
+            // in the list print out the index of the number
+
+
+
+
+
+
+
+           
+        }
+    }
+}
